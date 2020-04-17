e4fcc673d1f3c64e7cc36123f1f93e023d6eebb4
nshou <nshou@coronocoya.net>
2020-04-17 14:50:33 -0400

    Using map to keep write operation detail instead of using global static variable

--- deepstrings.cpp
+++ deepstrings.cpp
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unordered_map>
 #include "pin.H"
 
 static FILE *out;
 static FILE *debugout;
 
-// Variables to store values temporally
-static VOID *dst;
-static INT32 size;
+struct wop_detail{
+    VOID *dst;
+    INT32 size;
+};
+
+// Stores write operation details temporally
+std::unordered_map<void *, struct wop_detail *> wopmap;
 
 static void debugprint(VOID *ip){
+    VOID *dst = wopmap[ip]->dst;
+    INT32 size = wopmap[ip]->size;
+
     fprintf(debugout, "ip:%p dst:%p sz:%d val:0x", ip, dst, size);
     if(size == 1){
         fprintf(debugout, "%x", *((unsigned char *)dst));
@@ -26,18 +35,27 @@ static void debugprint(VOID *ip){
     fprintf(debugout, "\n");
 }
 
-static VOID memwriteop(VOID *ip){
-    debugprint(ip);
+static VOID savewop(VOID *ip, VOID *addr, INT32 sz){
+    struct wop_detail *wop = (struct wop_detail *)malloc(sizeof(struct wop_detail));
+    wop->dst = addr;
+    wop->size = sz;
+    wopmap[ip] = wop;
 }
 
-static VOID cachedata(VOID *addr, INT32 sz){
-    dst = addr;
-    size = sz;
+static void forgetwop(VOID *ip){
+    free(wopmap[ip]);
+    wopmap.erase(ip);
+}
+
+static VOID memwriteop(VOID *ip){
+    debugprint(ip);
+    forgetwop(ip);
 }
 
 VOID instruction(INS ins, VOID *v){
     if(INS_IsMemoryWrite(ins) && INS_IsStandardMemop(ins)){
-        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)cachedata,
+        INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)savewop,
+                                 IARG_INST_PTR,
                                  IARG_MEMORYWRITE_EA,
                                  IARG_MEMORYWRITE_SIZE,
                                  IARG_END);

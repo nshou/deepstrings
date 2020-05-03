caa645bf891bee4167704b441d9838d9b0d756da
nshou <nshou@coronocoya.net>
2020-05-02 20:33:49 -0400

    Implemented redundancy checks in terms of operations and data

--- deepstrings.cpp
+++ deepstrings.cpp
@@ -1,14 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unordered_map>
 #include "pin.H"
 
 static FILE *out;
 static unsigned long maxlen = 128;
-static unsigned long minlen = 3; //TODO: should be configurable
+static unsigned long minlen = 8; //TODO: should be configurable
+static std::unordered_map<void *, char *> rop_history;
+static std::unordered_map<char *, char *> data_history;
 
 static int iscommonascii(unsigned char ch){
     return (0x20 <= ch && ch <= 0x7e) || ch == 0x09 || ch == 0x0a || ch == 0x0d;
+}
+
+static int dataredundancy(char *c){
+    char *_c;
+    int ret = 0;
+
+    if(data_history.find(c) != data_history.end()){
+        _c = data_history[c];
+        if(strcmp(_c, c) == 0){
+            ret = 1;
+        }else{
+            free(_c);
+            data_history[c] = strdup(c);
+        }
+    }else{
+        data_history[c] = strdup(c);
+    }
+
+    return ret;
+}
+
+static int operationalredundancy(void *ip, char *c){
+    char *sub, *_c;
+    int ret = 0;
+
+    if(rop_history.find(ip) != rop_history.end()){
+        _c = rop_history[ip];
+        sub = strstr(_c, c);
+        if(sub != NULL && sub == c && strlen(c) + (int)(sub - _c) == strlen(_c)){
+            ret = 1;
+        }
+    }
+
+    rop_history[ip] = c;
+    return ret;
+}
+
+static int isredundant(void *ip, char *c){
+    int datar = dataredundancy(c);
+    int oper = operationalredundancy(ip, c);
+    return datar || oper;
 }
 
 static VOID onread(VOID *ip, VOID *addr){
@@ -21,9 +65,15 @@ static VOID onread(VOID *ip, VOID *addr){
         }
     }
 
-    if(i >= minlen){
-        fprintf(out, "rdip:%p addr:%p len:%lu str:%s\n", ip, addr, i, c);
+    if(i < minlen){
+        return;
     }
+
+    if(isredundant(ip, c)){
+        return;
+    }
+
+    fprintf(out, "rdip:%p addr:%p len:%lu str:%s\n", ip, addr, i, c);
 }
 
 VOID instruction(INS ins, VOID *v){

f1061486912d86598059f106cd6478d1da1e118f
nshou <nshou@coronocoya.net>
2020-07-24 02:44:53 -0400

    Implemented DCDict data structure

--- deepstrings.cpp
+++ deepstrings.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unordered_map>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
 #include "pin.H"
 
 static FILE *out;
@@ -11,6 +14,108 @@ static unsigned long minlen = 8; //TODO: these should be configurable
 static std::unordered_map<void *, char *> rop_history;
 static std::unordered_map<char *, char *> data_history;
 static char *floatstr;
+
+static unsigned long bisect_right(std::vector<unsigned long> v, unsigned long x){
+    unsigned long lo = 0;
+    unsigned long hi = v.size();
+    unsigned long mid;
+
+    while(lo < hi){
+        mid = (lo + hi) / 2;
+        if(x < v[mid]){
+            hi = mid;
+        }else{
+            lo = mid + 1;
+        }
+    }
+
+    return lo;
+}
+
+static unsigned long bisect_left(std::vector<unsigned long> v, unsigned long x){
+    unsigned long lo = 0;
+    unsigned long hi = v.size();
+    unsigned long mid;
+
+    while(lo < hi){
+        mid = (lo + hi) / 2;
+        if(v[mid] < x){
+            lo = mid + 1;
+        }else{
+            hi = mid;
+        }
+    }
+
+    return lo;
+}
+
+class DCDict{
+private:
+    std::vector<unsigned long> keyblks;
+    std::vector<void *> values;
+
+public:
+    void *get(unsigned long keyblk_l, unsigned long keyblk_r, void *dfl){
+        unsigned long lpos;
+        unsigned long rpos;
+
+        if(keyblk_l > keyblk_r){
+            return dfl;
+        }
+
+        lpos = bisect_right(keyblks, keyblk_l);
+        rpos = bisect_left(keyblks, keyblk_r);
+
+        if(keyblk_l == keyblk_r && lpos != rpos){
+            // len of key to search is 1 and the key is on the edge of a block.
+            return values[std::min(lpos, rpos) / 2];
+        }else if(lpos == rpos && lpos % 2 == 1){
+            return values[(lpos - 1) / 2];
+        }
+
+        return dfl;
+    }
+
+    void del(unsigned long keyblk_l, unsigned long keyblk_r){
+        unsigned long lpos;
+        unsigned long rpos;
+        unsigned long ldel;
+        unsigned long rdel;
+
+        if(keyblk_l > keyblk_r){
+            return;
+        }
+
+        lpos = bisect_left(keyblks, keyblk_l);
+        rpos = bisect_right(keyblks, keyblk_r);
+
+        if(lpos != rpos || lpos % 2 !=0){
+            ldel = 2 * (lpos / 2);
+            rdel = 2 * ((rpos + 1) / 2);
+            keyblks.erase(keyblks.begin() + ldel, keyblks.begin() + rdel);
+            values.erase(values.begin() + ldel / 2, values.begin() + rdel / 2);
+            //TODO: free val
+        }
+    }
+
+    void set(unsigned long keyblk_l, unsigned long keyblk_r, void *value){
+        unsigned long pos;
+
+        if(keyblk_l > keyblk_r){
+            return;
+        }
+
+        del(keyblk_l, keyblk_r);
+        pos = bisect_right(keyblks, keyblk_l);
+        keyblks.insert(keyblks.begin() + pos, keyblk_r);
+        keyblks.insert(keyblks.begin() + pos, keyblk_l);
+        values.insert(values.begin() + pos / 2, value);
+    }
+
+    ~DCDict(){
+        //TODO: free vals
+    }
+};
 
 static int iscommonascii(unsigned char ch){
     return (0x20 <= ch && ch <= 0x7e) || ch == 0x09 || ch == 0x0a || ch == 0x0d;
